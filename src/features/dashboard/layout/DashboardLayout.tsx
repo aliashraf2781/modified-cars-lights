0c93ac9d13d99b7052c8e64345940c92e4eba1d3
Ali Ashraf <aaaaaa30101975@gmail.com>
2025-12-03 00:43:37 +0200

    feat:dashboard ui

--- src/features/dashboard/layout/DashboardLayout.tsx
+++ src/features/dashboard/layout/DashboardLayout.tsx
@@ -2,11 +2,11 @@ import { Outlet } from "react-router-dom";
 
 export default function DashboardLayout() {
     return (
-        <div className="min-h-screen bg-gray-100">
-            <header className="bg-white shadow p-4">
-                <h1 className="text-xl font-bold">Dashboard</h1>
+        <div className="min-h-screen bg-neutral-900">
+            <header className="bg-white/5  shadow p-4">
+                <h1 className="text-2xl text-red-500 font-bold">Dashboard</h1>
             </header>
-            <main className="p-4">
+            <main>
                 <Outlet />
             </main>
         </div>
